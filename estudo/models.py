528fb652864814a3f249971e3fc5029f4668c024
CaaCesar <kcesarcg2000@gmail.com>
2025-10-31 16:45:09 -0300

    Models

---
+++ estudo/models.py
@@ -0,0 +1,13 @@
+from estudo import db
+from datetime import datetime
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    assunto = db.Column(db.String(150), nullable=True)
+    mensagem = db.Column(db.String(500), nullable=True)
+    data_envio = db.Column(db.DateTime, default=datetime.utcnow())
+    respondido = db.Column(db.Integer, default=0)
+
+
+    
